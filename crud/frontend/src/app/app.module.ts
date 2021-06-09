0fa3dc3e16cadd346a6c7535cbaced81c2ec8c54
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-06-09 07:31:53 -0300

    components read produto

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { MatInputModule } from '@angular/material/input';
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
